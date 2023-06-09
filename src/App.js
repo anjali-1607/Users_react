a5aa7b56e575916727d5285621c0b30a8a25df23
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-09 13:14:10 +0530

    user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Icon, Card } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="navbar">
+        <Button primary icon labelPosition="left">
+          <Icon name="all users" />
+          All User's
+        </Button>
+        <Button secondary icon labelPosition="right">
+          Add User's
+          <Icon name="add users" />
+        </Button>
+      </div>
+
+      <div className="card">
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <Card.Header>Matthew Harris</Card.Header>
+              <Card.Meta>Co-Worker</Card.Meta>
+              <Card.Description>
+                Matthew is a pianist living in Nashville.
+              </Card.Description>
+            </Card.Content>
+          </Card>
+
+          <Card>
+            <Card.Content>
+              <Card.Header content="Jake Smith" />
+              <Card.Meta content="Musicians" />
+              <Card.Description content="Jake is a drummer living in New York." />
+            </Card.Content>
+          </Card>
+
+          <Card>
+            <Card.Content
+              header="Elliot Baker"
+              meta="Friend"
+              description="Elliot is a music producer living in Chicago."
+            />
+          </Card>
+
+          <Card
+            header="Jenny Hess"
+            meta="Friend"
+            description="Jenny is a student studying Media Management at the New School"
+          />
+        </Card.Group>
+      </div>
+    </>
   );
 }
 
---
+++ src/Info.js
@@ -0,0 +1,26 @@
+let info = [
+  {
+    id: 1,
+    name: "Anjali Rajpoot",
+    profession: "Student",
+    email: "anjalirajpoot1609@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Rishabh Sahu",
+    profession: "Student",
+    email: "rishabhsahu0702@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Golu Rajpoot",
+    profession: "Student",
+    email: "golurajpoot0611@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Raavi Rajpoot",
+    profession: "Student",
+    email: "raavirajpoot2612@gmail.com",
+  },
+];
